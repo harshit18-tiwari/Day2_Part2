f729476686b73cdeaffd44c891bb55d87341f337
harshit18-tiwari <harshitoff18@gmail.com>
2026-01-19 15:01:22 +0530

    Initial commit

---
+++ src/main/java/com/example/day2_part2/models/StudentModel.java
@@ -0,0 +1,37 @@
+package com.example.day2_part2.models;
+
+public class StudentModel {
+    private int id;
+    private String name;
+    private String email;
+
+    public StudentModel(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
