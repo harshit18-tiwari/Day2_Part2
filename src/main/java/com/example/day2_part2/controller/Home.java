b4aac7aa7664b9e715aaf3c0af73ad82a9eecfd3
harshit18-tiwari <harshitoff18@gmail.com>
2026-01-19 15:15:06 +0530

    First API

--- src/main/java/com/example/day2_part2/controller/Home.java
+++ src/main/java/com/example/day2_part2/controller/Home.java
@@ -1,13 +1,16 @@
 package com.example.day2_part2.controller;
 
 
+import com.example.day2_part2.models.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Home {
-    @GetMapping("/about")
-    public String about() {
-        return "This is the about page";
+    @GetMapping("/")
+    public StudentModel getStudent() {//return the object of StudentModel
+        StudentModel studet = new StudentModel(1,"Harshit","harshit@gmail.com");
+        return studet;
     }
+
 }
